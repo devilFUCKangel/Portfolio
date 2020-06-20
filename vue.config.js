eb7239ccda50009a9b4eaefd8a6c98145c2ad279
devilFUCKangel <devilangel12345@live.com>
2020-06-21 00:03:08 +0800

    Invoke and TryAgainInYarn
    I dont know why.
    I add plugin with yarn but yarn advised not to mix package managers (npm). It took sometime to resolve packages (give alot warning).
    
    Deploying 0 assets from D:\Learn_and_Try\Vue\my-app\dist\ to https://s3-ap-southeast-1.amazonaws.com/vuecliplugins3bucket/
    
    Yarn installation cause alot problem, I use npm run deploy. But its same, 0 asset.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'devilFUCKangel',
+      overrideEndpoint: false,
+      region: 'ap-southeast-1',
+      bucket: 'vuecliplugins3bucket',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
 }
